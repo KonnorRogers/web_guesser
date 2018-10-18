f17c04449846d2c7ad4484bddc73b0bbeb63407d
paramagician <konnor5456@gmail.com>
2018-10-18 02:58:44 +0000

    Added random number, iteration 0

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 require 'sinatra'
+require 'sinatra/reloader'
 
+# generate random number
+r_num = rand 100
+
+# Base request to server
 get '/' do
-  'Hello, world!'
+  "The secret number is #{r_num}"
 end
