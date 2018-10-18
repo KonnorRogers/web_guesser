087c9cf7f5de95b661748074c33d41c9f6ab8119
paramagician <konnor5456@gmail.com>
2018-10-18 03:01:13 +0000

    It generates a random number & reloads when changes are made

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 require 'sinatra'
-require 'sinatra/reloader'
+require 'sinatra/reloader' # Automatically reloads the server when changes are made
 
 # generate random number
 r_num = rand 100
 
 # Base request to server
 get '/' do
-  "The secret number is #{r_num}"
+  "The SECRET NUMBER is #{r_num}"
 end
