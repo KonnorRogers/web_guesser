e3a0d10c34790355e835f5d4c75d48f08800193a
paramagician <konnor5456@gmail.com>
2018-10-18 07:37:12 +0000

    Got it all working as intended, minor hacks involved, but it works

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,55 +3,105 @@
 require 'sinatra'
 require 'sinatra/reloader' # Automatically reloads the server when changed
 
-def guess_logic(guess)
-  if guess > SECRET_NUMBER + 5
-    :too_high
-  elsif guess > SECRET_NUMBER && guess <= SECRET_NUMBER + 5
-    :high
-  elsif guess == SECRET_NUMBER
-    :correct
-  elsif guess < SECRET_NUMBER && guess >= SECRET_NUMBER - 5
-    :low
-  elsif guess < SECRET_NUMBER - 5
-    :too_low
+# set :secret_number, (proc { rand(100) })
+
+# SECRET_NUMBER = settings.secret_number
+
+class Game
+  MAX_GUESSES = 5
+
+  attr_accessor :guesses
+  attr_reader :secret_num, :has_reset, :cheat, :won
+
+  def initialize
+    @guesses = MAX_GUESSES
+    @secret_num = rand(100)
+    @has_reset = false
+    @cheat = false
+    @won = false
+  end
+
+  def guess_logic(guess)
+    return :too_high if guess > @secret_num + 5
+    return :high if guess > @secret_num && guess <= @secret_num + 5
+    return :correct if guess == @secret_num
+    return :low if guess < @secret_num && guess >= @secret_num - 5
+    return :too_low if guess < @secret_num - 5
+  end
+
+  def check_guess(how_close)
+    case how_close
+    when :too_high then 'Way too high!'
+    when :high then 'Too high!'
+    when :correct then @won = true && 'You got it right! The game will reset!'
+    when :low then 'Too low!'
+    when :too_low then 'Way too low!'
+    end
+  end
+
+  def background(how_close)
+    too_high_low = %i[too_high too_low]
+    return 'red' if too_high_low.include?(how_close)
+
+    high_low = %i[high low]
+    return '#F5A9A9' if high_low.include?(how_close) # light red
+    return 'green' if how_close == :correct
+
+    'white'
+  end
+
+  def too_many_guesses?(guess)
+    return true if @guesses.zero? && guess != @secret_num
+
+    false
+  end
+
+  def remove_guess
+    @guesses -= 1
+    @has_reset = false
+    @won = false
+  end
+
+  def reset
+    @has_reset = true
+    @secret_num = rand(100)
+    @guesses = MAX_GUESSES
+    disable_cheat
+  end
+
+  def reset_message
+    "You have guessed incorrectly too many times. The game has been reset and a new number generated."
+  end
+
+  def enable_cheat
+    @cheat = true
+  end
+
+  def disable_cheat
+    @cheat = false
   end
 end
 
-def check_guess(how_close)
-  case how_close
-  when :too_high
-    'Way too high!'
-  when :high
-    'Too high!'
-  when :correct
-    'You got it right!'
-  when :low
-    'Too low!'
-  when :too_low
-    'Way too low!'
-  end
-end
+game = Game.new
 
-def background(how_close)
-  too_high_low = %i[too_high too_low]
-  return 'red' if too_high_low.include?(how_close)
-
-  high_low = %i[high low]
-  return '#F5A9A9' if high_low.include?(how_close) # light red
-  return 'green' if how_close == :correct
-end
-
-set :secret_number, (proc { rand(100) })
-
-SECRET_NUMBER = settings.secret_number
-# Base request to server
+# Base request to the server
 get '/' do
+  game.disable_cheat
+  game.enable_cheat if params['cheat'] == 'true'
+
+  game.reset if game.won
+  game.remove_guess unless params['guess'].nil?
+
+
   guess = params['guess'].to_i
-  how_close = guess_logic(guess)
-  message = check_guess(how_close)
+  game.reset if game.too_many_guesses?(guess)
+  how_close = game.guess_logic(guess) unless game.has_reset
   bg_color = background(how_close)
+  message = game.check_guess(how_close)
+
 
   erb :index, locals: {
+    game: game,
     guess: guess,
     message: message,
     how_close: how_close,
