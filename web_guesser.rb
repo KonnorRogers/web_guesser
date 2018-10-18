2cd22f996a32aebef8e61d33a5965921739ce5b4
paramagician <konnor5456@gmail.com>
2018-10-18 05:49:18 +0000

    Added changing background based on guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,18 +3,42 @@
 require 'sinatra'
 require 'sinatra/reloader' # Automatically reloads the server when changed
 
-def check_guess(guess)
+def guess_logic(guess)
   if guess > SECRET_NUMBER + 5
-    'Way too high!'
+    :too_high
   elsif guess > SECRET_NUMBER && guess <= SECRET_NUMBER + 5
-    'Too high!'
+    :high
   elsif guess == SECRET_NUMBER
-    'You got it right!'
+    :correct
   elsif guess < SECRET_NUMBER && guess >= SECRET_NUMBER - 5
-    'Too low!'
+    :low
   elsif guess < SECRET_NUMBER - 5
+    :too_low
+  end
+end
+
+def check_guess(how_close)
+  case how_close
+  when :too_high
+    'Way too high!'
+  when :high
+    'Too high!'
+  when :correct
+    'You got it right!'
+  when :low
+    'Too low!'
+  when :too_low
     'Way too low!'
   end
+end
+
+def background(how_close)
+  too_high_low = %i[too_high too_low]
+  return 'red' if too_high_low.include?(how_close)
+
+  high_low = %i[high low]
+  return '#F5A9A9' if high_low.include?(how_close) # light red
+  return 'green' if how_close == :correct
 end
 
 set :secret_number, (proc { rand(100) })
@@ -23,8 +47,16 @@ SECRET_NUMBER = settings.secret_number
 # Base request to server
 get '/' do
   guess = params['guess'].to_i
-  message = check_guess(guess)
-  erb :index, locals: { guess: guess, message: message }
+  how_close = guess_logic(guess)
+  message = check_guess(how_close)
+  bg_color = background(how_close)
+
+  erb :index, locals: {
+    guess: guess,
+    message: message,
+    how_close: how_close,
+    bg_color: bg_color
+  }
 end
 #
 # set :foo, 'bar'
